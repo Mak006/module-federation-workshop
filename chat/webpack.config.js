7b7f1e6e112864676711d33d87c9758187950693
Aman Beg <aman_b@hotmail.com>
2021-10-19 21:52:59 -0400

    working

--- chat/webpack.config.js
+++ chat/webpack.config.js
@@ -1,4 +1,5 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
 
 module.exports = {
   mode: "development",
@@ -9,5 +10,12 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "./public/index.html",
     }),
+    new ModuleFederationPlugin({
+      name: "chat",
+      filename: "remoteEntry.js",
+      exposes: {
+          "./App": "./src/index.js",
+      },
+  }),
   ],
 };
